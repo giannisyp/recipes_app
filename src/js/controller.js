6d1c48bfbf3e30ebbbba2b57a5d0737a953b8197
Ypaspistis Giannis <giannisypaspistis@gmail.com>
2024-08-04 16:37:46 +0300

    adding async

--- src/js/controller.js
+++ src/js/controller.js
@@ -7,7 +7,7 @@ import bookmarksView from './views/bookmarksView.js';
 import addRecipeView from './views/addRecipeView.js';
 import { MODAL_CLOSE_SEC } from './config.js';
 import 'core-js/stable';
-import 'regenerator-runtime/runtime';
+import { async } from 'regenerator-runtime';
 
 const controlRecipes = async function () {
   try {
